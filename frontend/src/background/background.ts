20f6d374135a6817af81f161bb1d0d5724c1e609
SophieClaxton <sophie.e.claxton@gmail.com>
2024-10-30 20:14:16 +0000

    MIBE-19: fix: opening sidepanel via action changes context menu

--- frontend/src/background/background.ts
+++ frontend/src/background/background.ts
@@ -1,20 +1,28 @@
 import { ContextMenuActions, openSidePanelAction, closeSidePanelAction } from './contextMenuActions';
 import { MessageType, Port } from '../common/message';
 
-chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: true });
+/*
+Chrome only allows one action item on the main context menu per extension.
+When there are multiple context menu actions, they are nested under a single
+parent.
+
+Only one context menu action is valid at a time, so to avoid unnecessary
+nesting, the actions are removed and re-created each time they are swapped.
+*/
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create(openSidePanelAction);
 });
 
-/*
-  Chrome only allows one action item on the main context menu per extension.
-  When there are multiple context menu actions, they are nested under a single
-  parent.
+/* Disable the automatic action configuration, so that it can be customised */
+chrome.sidePanel.setPanelBehavior({ openPanelOnActionClick: false });
 
-  Only one context menu action is valid at a time, so to avoid unnecessary
-  nesting, the actions are removed and re-created each time they are swapped.
-*/
+chrome.action.onClicked.addListener((tab) => {
+  chrome.contextMenus.removeAll();
+  chrome.contextMenus.create(closeSidePanelAction);
+  chrome.sidePanel.open({ windowId: tab!.windowId });
+});
+
 chrome.contextMenus.onClicked.addListener((info, tab) => {
   if (info.menuItemId === ContextMenuActions.OpenSidePanel) {
     chrome.sidePanel.open({ windowId: tab!.windowId });
