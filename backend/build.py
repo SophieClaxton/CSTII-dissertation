638aba5f3a3617b8b3c3215b9f831ca468f2b09e
SophieClaxton <sophie.e.claxton@gmail.com>
2024-12-18 22:32:13 +0000

    MIBE-44: add python build script to test server

---
+++ backend/build.py
@@ -0,0 +1,36 @@
+import subprocess
+import time
+import requests
+
+
+def build_and_close():
+    try:
+        server_process = subprocess.Popen(
+            ["fastapi", "dev", "app/main.py"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        time.sleep(2)
+
+        status_check_response = requests.get("http://localhost:8000/")
+        status_check = status_check_response.json()
+        if (status_check_response.status_code != 200) or (
+            status_check["status"] != "live"
+        ):
+            raise Exception("Status check failed")
+
+        server_process.terminate()
+        server_process.wait()
+
+        print("Build successful")
+        return 0
+
+    except Exception as e:
+        print(f"Build failed: {e}")
+        return 1
+
+
+if __name__ == "__main__":
+    exit_code = build_and_close()
+    exit(exit_code)
