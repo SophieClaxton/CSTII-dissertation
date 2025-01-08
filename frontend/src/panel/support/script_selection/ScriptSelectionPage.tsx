71ca29b3de055985502cc418fb6dbe703e5163cd
SophieClaxton <sophie.e.claxton@gmail.com>
2025-01-08 18:16:17 +0000

    MIBE-45: feat: refactor to separate out dialog

--- frontend/src/panel/support/script_selection/ScriptSelectionPage.tsx
+++ frontend/src/panel/support/script_selection/ScriptSelectionPage.tsx
@@ -7,21 +7,12 @@ import { getScripts } from '../../api/scripts';
 import ScriptListItem from './ScriptListItem';
 import './styles/scriptSelectionPage.css';
 import List from '@mui/material/List/List';
-import {
-  Autocomplete,
-  Button,
-  Chip,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
-  Stack,
-  TextField,
-} from '@mui/material';
+import { Button, Chip, Stack } from '@mui/material';
 import Website from '../../models/Website';
 import User from '../../models/User';
 import { getWebsites } from '../../api/websites';
 import { getUsers } from '../../api/users';
+import ScriptFilterDialog from './ScriptFilterDialog';
 
 const ScriptSelectionPage: React.FC = () => {
   const { removeCurrentScreen } = useNavigationContext();
@@ -77,9 +68,6 @@ const ScriptSelectionPage: React.FC = () => {
     }
   }, [scriptsData, websiteFilters, authorFilters]);
 
-  const [selectedWebsite, setSelectedWebsite] = useState<Website | null>(null);
-  const [selectedAuthor, setSelectedAuthor] = useState<User | null>(null);
-
   const scriptsContent =
     scriptsData.status === 'Loading' ? (
       <div>Loading...</div>
@@ -133,77 +121,16 @@ const ScriptSelectionPage: React.FC = () => {
         </button>
       </div>
       <div className="all-scripts-container">{scriptsContent}</div>
-      <Dialog
+      <ScriptFilterDialog
         open={openFilterDialog}
-        onClose={() => setOpenFilterDialog(false)}
-        fullWidth={true}
-        maxWidth={'sm'}
-      >
-        <DialogTitle>Add Filter</DialogTitle>
-        <DialogContent>
-          <Stack spacing={1} sx={{ marginTop: '0.5rem' }}>
-            <Stack
-              direction={'row'}
-              justifyContent={'space-between'}
-              spacing={2}
-            >
-              <Autocomplete
-                value={selectedWebsite}
-                options={websites}
-                getOptionLabel={(website) => website.url}
-                renderInput={(params) => (
-                  <TextField {...params} label="Choose Website" />
-                )}
-                autoComplete={true}
-                autoSelect={true}
-                onChange={(_event, value) => setSelectedWebsite(value)}
-                sx={{ width: '100%' }}
-              />
-              <Button
-                onClick={() => {
-                  if (selectedWebsite) {
-                    setWebsiteFilters([...websiteFilters, selectedWebsite]);
-                    setSelectedWebsite(null);
-                  }
-                }}
-              >
-                Add
-              </Button>
-            </Stack>
-            <Stack
-              direction={'row'}
-              justifyContent={'space-between'}
-              spacing={2}
-            >
-              <Autocomplete
-                value={selectedAuthor}
-                options={authors}
-                getOptionLabel={(author) => author.name}
-                renderInput={(params) => (
-                  <TextField {...params} label="Choose Author" />
-                )}
-                autoComplete={true}
-                autoSelect={true}
-                onChange={(_event, value) => setSelectedAuthor(value)}
-                sx={{ width: '100%' }}
-              />
-              <Button
-                onClick={() => {
-                  if (selectedAuthor) {
-                    setAuthorFilters([...authorFilters, selectedAuthor]);
-                    setSelectedAuthor(null);
-                  }
-                }}
-              >
-                Add
-              </Button>
-            </Stack>
-          </Stack>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={() => setOpenFilterDialog(false)}>Close</Button>
-        </DialogActions>
-      </Dialog>
+        setOpen={setOpenFilterDialog}
+        websites={websites}
+        authors={authors}
+        websiteFilters={websiteFilters}
+        setWebsiteFilters={setWebsiteFilters}
+        authorFilters={authorFilters}
+        setAuthorFilters={setAuthorFilters}
+      />
     </div>
   );
 };
