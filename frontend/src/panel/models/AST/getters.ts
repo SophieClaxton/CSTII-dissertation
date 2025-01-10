2df55af34790e256dfc152ee04294a7712039810
SophieClaxton <sophie.e.claxton@gmail.com>
2025-01-10 15:47:47 +0000

    MIBE-49: feat: show errors if API returns null for required value

--- frontend/src/panel/models/AST/getters.ts
+++ frontend/src/panel/models/AST/getters.ts
@@ -3,7 +3,7 @@ const getMissingProperties = <I extends object, R extends object>(
   schema: Record<keyof R, boolean>,
 ): string[] => {
   return Object.keys(schema)
-    .filter((key: string) => !(key in node))
+    .filter((key: string) => !(key in node) || node[key as keyof I] === null)
     .map((key) => key as keyof R)
     .filter((key: keyof R) => schema[key])
     .map((key) => `Missing ${key.toString()}`);
